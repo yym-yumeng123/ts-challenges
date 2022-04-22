1ad5c2afe8a602890a87870b8c683a060d892540
yym-yumeng123 <18026493873@163.com>
2022-04-22 17:49:56 +0800

    infer 推断, 递归

---
+++ types/898-easy-includes/template.ts
@@ -0,0 +1,13 @@
+import type { Equal } from "@type-challenges/utils";
+
+// 利用infer取到数组的第一个，和U对比。相等的话说明U存在于T直接返回true，不相等则用数组剩余部分递归执行Includes
+type Includes<T extends readonly any[], U> = T extends [
+  infer First,
+  ...infer Rest
+]
+  ? Equal<U, First> extends true
+    ? true
+    : Includes<Rest, U>
+  : false;
+
+type 啊 = Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Kars">;
