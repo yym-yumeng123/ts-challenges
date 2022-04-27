aa7174ebda79a12454193b7dc43b1556dafc44ad
yym-yumeng123 <18026493873@163.com>
2022-04-27 11:35:36 +0800

    优化嵌套 Promise

--- types/189-easy-awaited/template.ts
+++ types/189-easy-awaited/template.ts
@@ -1,1 +1,10 @@
-type MyAwaited<T> = T extends Promise<infer K> ? MyAwaited<K> : T;
+type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer K>
+  ? K extends Promise<unknown>
+    ? MyAwaited<K>
+    : K
+  : never;
+
+/**
+ * 1. infer 条件类型
+ * 2. 设置未知数
+ */
